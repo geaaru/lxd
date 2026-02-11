lxc/incus

tests: Add OpenID Connect tests
Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

test/suites/oidc.sh
@@ -1,0 +1,26 @@
+test_oidc() {
+  ensure_import_testimage
+
+  # shellcheck disable=2153
+  ensure_has_localhost_remote "${INCUS_ADDR}"
+
+  # Setup OIDC
+  spawn_oidc
+  incus config set "oidc.issuer=http://127.0.0.1:$(cat "${TEST_DIR}/oidc.port")/"
+  incus config set "oidc.client.id=device"
+
+  BROWSER=curl incus remote add --accept-certificate oidc "${INCUS_ADDR}" --auth-type oidc
+  [ "$(incus info oidc: | grep ^auth_user_name | sed "s/.*: //g")" = "unknown" ]
+  incus remote remove oidc
+
+  set_oidc test-user
+
+  BROWSER=curl incus remote add --accept-certificate oidc "${INCUS_ADDR}" --auth-type oidc
+  [ "$(incus info oidc: | grep ^auth_user_name | sed "s/.*: //g")" = "test-user" ]
+  incus remote remove oidc
+
+  # Cleanup OIDC
+  kill_oidc
+  incus config unset oidc.issuer
+  incus config unset oidc.client.id
+}

geaaru/lxd

Signed-off-by: Mark Laing <mark.laing@canonical.com>

test/suites/oidc.sh
@@ -1,0 +1,26 @@
+test_oidc() {
+  ensure_import_testimage
+
+  # shellcheck disable=2153
+  ensure_has_localhost_remote "${LXD_ADDR}"
+
+  # Setup OIDC
+  spawn_oidc
+  lxc config set "oidc.issuer=http://127.0.0.1:$(cat "${TEST_DIR}/oidc.port")/"
+  lxc config set "oidc.client.id=device"
+
+  BROWSER=curl lxc remote add --accept-certificate oidc "${LXD_ADDR}" --auth-type oidc
+  [ "$(lxc info oidc: | grep ^auth_user_name | sed "s/.*: //g")" = "unknown" ]
+  lxc remote remove oidc
+
+  set_oidc test-user
+
+  BROWSER=curl lxc remote add --accept-certificate oidc "${LXD_ADDR}" --auth-type oidc
+  [ "$(lxc info oidc: | grep ^auth_user_name | sed "s/.*: //g")" = "test-user" ]
+  lxc remote remove oidc
+
+  # Cleanup OIDC
+  kill_oidc
+  lxc config unset oidc.issuer
+  lxc config unset oidc.client.id
+}