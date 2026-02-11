lxc/incus

tests: Fix syslog test
Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

test/suites/syslog.sh
@@ -1,9 +1,4 @@
 test_syslog_socket() {
-  INCUS_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
-  export INCUS_DIR
-  chmod +x "${INCUS_DIR}"
-  spawn_incus "${INCUS_DIR}" true
-
   incus config set core.syslog_socket=true
   incus monitor --type=network-acl > "${TEST_DIR}/ovn.log" &
   monitorOVNPID=$!
@@ -17,5 +12,5 @@ test_syslog_socket() {
   grep -qF "type: network-acl" "${TEST_DIR}/ovn.log"
   grep -qF "name=\"some-acl\"" "${TEST_DIR}/ovn.log"
 
-  shutdown_incus "${INCUS_DIR}"
+  incus config unset core.syslog_socket
 }

geaaru/lxd

Signed-off-by: Din Music <din.music@canonical.com>

test/suites/syslog.sh
@@ -1,9 +1,4 @@
 test_syslog_socket() {
-  LXD_DIR=$(mktemp -d -p "${TEST_DIR}" XXX)
-  export LXD_DIR
-  chmod +x "${LXD_DIR}"
-  spawn_lxd "${LXD_DIR}" true
-
   lxc config set core.syslog_socket=true
   lxc monitor --type=ovn > "${TEST_DIR}/ovn.log" &
   monitorOVNPID=$!
@@ -16,5 +11,5 @@ test_syslog_socket() {
   grep -qF "type: ovn" "${TEST_DIR}/ovn.log"
   grep -qF "unix:/var/run/openvswitch/br-int.mgmt: connected" "${TEST_DIR}/ovn.log"
 
-  shutdown_lxd "${LXD_DIR}"
+  lxc config unset core.syslog_socket
 }